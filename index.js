91983cb3e68e81b3c5f0efb013a168ab6f3fed89
Todd Wolfson <todd@twolfson.com>
2013-04-28 22:11:18 -0700

    Moved to require.resolve and added dev notes

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ var fs = require('fs');
 var processModulePath = require.resolve('process/browser.js');
 var processModuleSrc = fs.readFileSync(processModulePath, 'utf8');
 
-var bufferModulePath = path.join(__dirname, 'buffer.js');
+// DEV: Need to find out where the queue comes from and goes to and
+// what the deal is with the {id, source, deps}
+// as techincally, we should have a programatic require
+var bufferModulePath = require.resolve('buffer-browserify');
 var bufferModuleSrc = fs.readFileSync(bufferModulePath, 'utf8');
 
 var varNames = [ 'process', 'global', '__filename', '__dirname', 'Buffer' ];
@@ -20,7 +23,7 @@ module.exports = function (files, opts) {
     }
     if (!opts) opts = {};
     if (!files) files = [];
-    
+
     var basedir = opts.basedir || (files.length
         ? commondir(files.map(function (x) {
             return path.resolve(path.dirname(x));
@@ -28,9 +31,9 @@ module.exports = function (files, opts) {
         : '/'
     );
     var resolved = { process: false, Buffer: false };
-    
+
     return through(write, end);
-    
+
     function write (row) {
         if (!opts.always
             && !/\bprocess\b/.test(row.source)
@@ -39,13 +42,13 @@ module.exports = function (files, opts) {
             && !/\b__filename\b/.test(row.source)
             && !/\b__dirname\b/.test(row.source)
         ) return this.queue(row);
-        
+
         var scope = opts.always
             ? { globals: { implicit: varNames } }
             : parseScope(row.source)
         ;
         var globals = {};
-        
+
         if (scope.globals.implicit.indexOf('process') >= 0) {
             if (!resolved.process) {
                 this.queue({
@@ -54,7 +57,7 @@ module.exports = function (files, opts) {
                     deps: {}
                 });
             }
-            
+
             resolved.process = true;
             row.deps.__browserify_process = processModulePath;
             globals.process = 'require("__browserify_process")';
@@ -67,7 +70,7 @@ module.exports = function (files, opts) {
                     deps: {}
                 });
             }
-            
+
             resolved.Buffer = true;
             row.deps.__browserify_buffer = bufferModulePath;
             globals.Buffer = 'require("__browserify_buffer").Buffer';
@@ -83,11 +86,11 @@ module.exports = function (files, opts) {
             var dir = path.dirname('/' + path.relative(basedir, row.id));
             globals.__dirname = JSON.stringify(dir);
         }
-        
+
         row.source = closeOver(globals, row.source);
         this.queue(row);
     }
-    
+
     function end () {
         this.ended = true;
         this.queue(null);
